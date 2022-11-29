10fa2f84f274dd2ec6fda3ff7b2dc487ef13676f
Joshua Montellano <ijmontee@gmail.com>
2022-11-29 14:44:48 -0800

    adding first game api version

--- game-store/src/main/java/com/company/gamestore/controller/GameController.java
+++ game-store/src/main/java/com/company/gamestore/controller/GameController.java
@@ -1,4 +1,88 @@
 package com.company.gamestore.controller;
 
+import com.company.gamestore.model.Game;
+import com.company.gamestore.repository.GameControllerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
 public class GameController {
+    @Autowired
+    private GameControllerRepository gameRepo;
+
+    @PostMapping("/game")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Game createGame(@RequestBody Game game){
+        return gameRepo.save(game);
+    }
+
+    @GetMapping("/game/id/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public Game getGameById(@PathVariable Integer id){
+        Optional<Game> game = gameRepo.findById(id);
+
+        if(game.isPresent()){
+            return game.get();
+        }
+
+        return null;
+    }
+
+    @GetMapping("/game")
+    @ResponseStatus(HttpStatus.OK)
+    public List<Game> getAllGames(){
+        return gameRepo.findAll();
+    }
+
+    @PutMapping("/game")
+    @ResponseStatus(HttpStatus.OK)
+    public void updateGame(@RequestBody Game game){
+        Optional<Game> oldGame = gameRepo.findById(game.getId());
+        if(oldGame.isPresent()){
+            oldGame.get()
+                    .setDescription(game.getDescription())
+                    .setEsrbRating(game.getEsrbRating())
+                    .setPrice(game.getPrice())
+                    .setQuantity(game.getQuantity())
+                    .setStudio(game.getStudio())
+                    .setTitle(game.getTitle());
+            gameRepo.save(oldGame.get());
+        }
+    }
+
+    @DeleteMapping("/game")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteGame(@PathVariable Integer id){
+        gameRepo.deleteById(id);
+    }
+
+    @GetMapping("/game/studio/{studio}")
+    @ResponseStatus(HttpStatus.OK)
+    public List<Game> getGamesByStudio(@PathVariable String studio){
+        return gameRepo.findGamesByStudio(studio);
+    }
+
+    @GetMapping("/game/esrb/{esrb}")
+    @ResponseStatus(HttpStatus.OK)
+    public List<Game> getGamesByEsrbRating(@PathVariable String esrb){
+        return gameRepo.findGamesByEsrbRating(esrb);
+    }
+
+    @GetMapping("/game/title/{title}")
+    @ResponseStatus(HttpStatus.OK)
+    public Game getGameByTitle(@PathVariable String title){
+        Optional<Game> game = gameRepo.findGameByTitle(title);
+        if (game.isPresent()){
+            return game.get();
+        }
+
+        return null;
+    }
+
+
+
 }
--- game-store/src/main/java/com/company/gamestore/model/Game.java
+++ game-store/src/main/java/com/company/gamestore/model/Game.java
@@ -1,4 +1,114 @@
 package com.company.gamestore.model;
 
-public class Game {
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Objects;
+
+@Entity
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+@Table(name="game")
+public class Game implements Serializable {
+    @Id
+    @Column(name = "game_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String title;
+    private String esrbRating;
+    private String description;
+    private BigDecimal price;
+    private String studio;
+    private Integer quantity;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public Game setId(Integer id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public Game setTitle(String title) {
+        this.title = title;
+        return this;
+    }
+
+    public String getEsrbRating() {
+        return esrbRating;
+    }
+
+    public Game setEsrbRating(String esrbRating) {
+        this.esrbRating = esrbRating;
+        return this;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Game setDescription(String description) {
+        this.description = description;
+        return this;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public Game setPrice(BigDecimal price) {
+        this.price = price;
+        return this;
+    }
+
+    public String getStudio() {
+        return studio;
+    }
+
+    public Game setStudio(String studio) {
+        this.studio = studio;
+        return this;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public Game setQuantity(Integer quantity) {
+        this.quantity = quantity;
+        return this;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Game game = (Game) o;
+        return Objects.equals(getId(), game.getId()) && Objects.equals(getTitle(), game.getTitle()) && Objects.equals(getEsrbRating(), game.getEsrbRating()) && Objects.equals(getDescription(), game.getDescription()) && Objects.equals(getPrice(), game.getPrice()) && Objects.equals(getStudio(), game.getStudio()) && Objects.equals(getQuantity(), game.getQuantity());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getTitle(), getEsrbRating(), getDescription(), getPrice(), getStudio(), getQuantity());
+    }
+
+    @Override
+    public String toString() {
+        return "Game{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", esrbRating='" + esrbRating + '\'' +
+                ", description='" + description + '\'' +
+                ", price=" + price +
+                ", studio='" + studio + '\'' +
+                ", quantity=" + quantity +
+                '}';
+    }
 }
