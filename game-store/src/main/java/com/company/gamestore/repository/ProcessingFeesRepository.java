d4697b520b8a6427692fd78742cc4ac6340de7be
stinsfire <stinsfire007@gmail.com>
2022-11-30 00:25:18 -0500

    minor updates
    minor updates to repositories

--- game-store/src/main/java/com/company/gamestore/repository/ProcessingFeesRepository.java
+++ game-store/src/main/java/com/company/gamestore/repository/ProcessingFeesRepository.java
@@ -1,10 +1,11 @@
 package com.company.gamestore.repository;
 
+import com.company.gamestore.model.ProcessingFee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.math.BigDecimal;
 import java.util.Optional;
 
-public interface ProcessingFeesRepository extends JpaRepository {
+public interface ProcessingFeesRepository extends JpaRepository <ProcessingFee, String> {
     Optional<BigDecimal> findProcessingFeesByProduct (String product);
 }
--- game-store/src/main/java/com/company/gamestore/repository/SalesTaxRateRepository.java
+++ game-store/src/main/java/com/company/gamestore/repository/SalesTaxRateRepository.java
@@ -1,6 +1,5 @@
 package com.company.gamestore.repository;
 
-import com.company.gamestore.model.Console;
 import com.company.gamestore.model.SalesTaxRate;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -8,6 +7,6 @@ import java.math.BigDecimal;
 import java.util.List;
 import java.util.Optional;
 
-public interface SalesTaxRateRepository extends JpaRepository {
+public interface SalesTaxRateRepository extends JpaRepository <SalesTaxRate, String>{
     Optional<BigDecimal> findSalesTaxRateByState (String state);
 }
