5b0731dd8f1b1149e1d941ecfbb9d99d7944a1e3
Joshua Montellano <ijmontee@gmail.com>
2022-12-01 15:16:12 -0800

    template for the service layer tests

--- game-store/src/test/java/com/company/gamestore/service/ServiceLayerTest.java
+++ game-store/src/test/java/com/company/gamestore/service/ServiceLayerTest.java
@@ -1,7 +1,24 @@
 package com.company.gamestore.service;
 
+import com.company.gamestore.repository.*;
+import org.junit.Before;
+
 import static org.junit.Assert.*;
 
 public class ServiceLayerTest {
 
+    ServiceLayer service;
+    ConsoleControllerRepository consoleControllerRepository;
+    GameControllerRepository gameControllerRepository;
+    InvoiceRepository invoiceRepository;
+    ProcessingFeesRepository processingFeesRepository;
+    SalesTaxRateRepository salesTaxRateRepository;
+    TshirtRepository tshirtRepository;
+
+    @Before
+    public void setUp() throws Exception{
+        //we will each att our set up mock to this funciton
+        service = new ServiceLayer(consoleControllerRepository,gameControllerRepository,invoiceRepository,
+                processingFeesRepository,salesTaxRateRepository,tshirtRepository);
+    }
 }
