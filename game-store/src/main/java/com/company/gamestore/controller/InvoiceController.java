43cef8bf55f8c6b7b9d880ed482073e9ab633454
Jose Salgado Jr <jjsalga1@uci.edu>
2022-12-01 03:59:20 -0800

    Added InvoiceViewModel and error handling

--- game-store/src/main/java/com/company/gamestore/controller/InvoiceController.java
+++ game-store/src/main/java/com/company/gamestore/controller/InvoiceController.java
@@ -1,59 +1,69 @@
 package com.company.gamestore.controller;
 
-import com.company.gamestore.model.Invoice;
-import com.company.gamestore.repository.InvoiceRepository;
+import com.company.gamestore.service.ServiceLayer;
+import com.company.gamestore.viewmodel.InvoiceViewModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 public class InvoiceController {
     @Autowired
-    InvoiceRepository repo;
+    private ServiceLayer serviceLayer;
 
     // Create a new invoice.
     @PostMapping("/invoice")
     @ResponseStatus(HttpStatus.CREATED)
-    public Invoice addInvoice(@RequestBody Invoice invoice) {
-        return repo.save(invoice);
+    public InvoiceViewModel addInvoice(@RequestBody InvoiceViewModel invoice) {
+        return serviceLayer.saveInvoice(invoice);
     }
 
     // Find invoice by id.
     @GetMapping("/invoice/{id}")
     @ResponseStatus(HttpStatus.OK)
-    public Invoice getInvoiceById(@PathVariable int id) {
-        Optional<Invoice> returnVal = repo.findById(id);
-        return returnVal.orElse(null);
+    public InvoiceViewModel getInvoiceById(@PathVariable int id) {
+        // Handle IVM not found
+        InvoiceViewModel foundIvm = serviceLayer.findInvoice(id);
+        if (foundIvm == null) {
+            throw new IllegalArgumentException("Invoice Error: Non-existent invoice.");
+        }
+        return foundIvm;
     }
 
     // Find invoices by customer name.
     @GetMapping("/invoice/name/{name}")
     @ResponseStatus(HttpStatus.OK)
-    public List<Invoice> getInvoiceByName(@PathVariable String name) {
-        return repo.findByName(name);
+    public List<InvoiceViewModel> getInvoiceByName(@PathVariable String name) {
+        // TODO: Comes back empty if no invoice found by name. Should we throw error?
+        return serviceLayer.findInvoicesByName(name);
     }
 
     // Get all invoices.
     @GetMapping("/invoice")
     @ResponseStatus(HttpStatus.OK)
-    public List<Invoice> getInvoices() {
-        return repo.findAll();
+    public List<InvoiceViewModel> getInvoices() {
+        return serviceLayer.findAllInvoices();
     }
 
     // Update an existing invoice.
     @PutMapping("/invoice")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void updateInvoice(@RequestBody Invoice invoice) {
-        repo.save(invoice);
+    public void updateInvoice(@RequestBody InvoiceViewModel invoice) {
+        // Handle IVM not found. Handled by check inside getInvoiceById method.
+        InvoiceViewModel ivmToUpdate = getInvoiceById(invoice.getId());
+        // Updated if found.
+        serviceLayer.updateInvoice(ivmToUpdate);
     }
 
     // Delete an existing invoice.
     @DeleteMapping("/invoice/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteInvoice(@PathVariable int id) {
-        repo.deleteById(id);
+        // Handle IVM not found. Handled by check inside getInvoiceById method.
+        InvoiceViewModel ivmToDelete = getInvoiceById(id);
+        // Deleted if found.
+        serviceLayer.removeInvoice(ivmToDelete.getId());
     }
 }
